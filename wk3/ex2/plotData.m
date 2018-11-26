8af88c427e0c71043ab6d8e3fe1ad663369d4600
cmseaton42 <cmseaton42@gmail.com>
2018-11-26 11:42:56 -0500

    Add plot code

--- wk3/ex2/plotData.m
+++ wk3/ex2/plotData.m
@@ -13,16 +13,14 @@ figure; hold on;
 %
 
 
+% Find Indices of Positive and Negative examples
+pos = find(y == 1); neg = find(y == 0);
 
-
-
-
-
-
+% Plot examples
+plot(X(pos, 1), X(pos, 2), 'k+', 'LineWidth', 2, 'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
 
 % =========================================================================
-
-
 
 hold off;
 
