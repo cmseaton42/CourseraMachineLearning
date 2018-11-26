4796b73afdf08113a323003deed283df4aad2243
cmseaton42 <cmseaton42@gmail.com>
2018-11-26 12:20:28 -0500

    Add sigmoid implementation

--- wk3/ex2/sigmoid.m
+++ wk3/ex2/sigmoid.m
@@ -9,9 +9,10 @@ g = zeros(size(z));
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
+g = 1 ./ (1 + e.^(-z));
 
-
-
+% Alternatively: You could use
+%    g = arrayfun(@(x) 1 / (1 + exp(-x)), z);
 
 % =============================================================
 
